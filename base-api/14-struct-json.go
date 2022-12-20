b827ae995ea36b824d7a6510198571d30060e2c3
dqt <229813475@qq.com>
2022-12-20 22:43:11 +0800

    添加fmt

---
+++ base-api/14-struct-json.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+/*
+	结构体字段的可见性与JSON序列化
+*/
+
+type People struct {
+	ID      int    //  对外可见
+	name    string // 对外不可见， 对当前包可见
+	Address string `json:"address"` //json的时候讲Address字段改为小写address, 这种方式叫做结构体tag
+}
+
+type Student struct {
+	ID   int
+	Name string
+}
+
+type class struct {
+	title    string // 因为是小写， 所以在json包里也是不可见的
+	Students []Student
+}
+
+// student 构造函数
+func newStudent(id int, name string) Student {
+	return Student{
+		ID:   id,
+		Name: name,
+	}
+}
+func main() {
+	fmt.Println("=======================分隔符: 结构体tag===========================")
+	people := People{
+		ID:      1,
+		name:    "ben",
+		Address: "杭州",
+	}
+	peopleStr, err := json.Marshal(people)
+	fmt.Printf("%s\n", peopleStr) // {"ID":1,"address":"杭州"}, name不可见且address为小写
+
+	fmt.Println("=======================分隔符:===========================")
+	// 创建一个班级
+	class1 := class{
+		title:    "小一班",
+		Students: make([]Student, 0, 20),
+	}
+
+	for i := 0; i < 10; i++ {
+		student := newStudent(i, fmt.Sprintf("stu%02d", i))
+		class1.Students = append(class1.Students, student)
+	}
+	fmt.Printf("%#v\n\r", class1)
+
+	fmt.Println("=======================分隔符===========================")
+
+	// json 序列化： 就是将go语言的数据 -> JSON格式的字符串
+	dataStr, err := json.Marshal(class1)
+	if err != nil {
+		fmt.Println("json序列化失败了")
+		return
+	}
+	fmt.Printf("%s\n", dataStr)
+
+	fmt.Println("=======================分隔符===========================")
+
+	// json反序列化： 就是将JSON字符串转为go语言的数据
+	// jsonStringify := `{"Title":"小一班","Students":[{"ID":0,"Name":"stu00"},{"ID":1,"Name":"stu01"},{"ID":2,"Name":"stu02"}]}`
+	var c2 class
+	err2 := json.Unmarshal([]byte(dataStr), &c2) // 需要字节类型的数据，因为要修改c2所以传入指针
+	if err2 != nil {
+		fmt.Println("json反序列化失败了")
+		return
+	}
+	fmt.Printf("%#v\n", c2)
+}
--- base-api/15-fmt.go
+++ base-api/15-fmt.go
@@ -8,9 +8,40 @@ type student struct {
 }
 
 func main() {
+	fmt.Print("直接输出", "\n")
+	fmt.Println("换行并输出")
+
+	// 格式化输出
+	fmt.Printf("姓名：%s, 年龄：%d\n", "alice", 17) // %s字符串占位， %d整形的占位
+
+	fmt.Printf("%s %d %t\n", "字符串", 17, true) // %t 布尔类型占位
+
+	fmt.Printf("%f\n", 3.1415) // %f 浮点类型占位
+
+	fmt.Printf("%.1f\n", 3.1415) // 指定精度， 保留一位小数
+
+	fmt.Printf("%6.1f\n", 3.1415) // 指定宽度和精度
+
 	student1 := student{
 		age:  12,
 		name: "ben",
 	}
 	fmt.Printf("年纪是：%s\n", student1.name)
+
+	// 输入， 获取用户输入值
+	// var name string
+	// var age int
+	var name2 string
+	var age2 int
+	// fmt.Print("请输入姓名：")
+	// fmt.Scan(&name)
+	// fmt.Print("请输入年龄：")
+	// fmt.Scan(&age)
+	// fmt.Printf("输入的姓名是：%s,输入的年龄是%d\n", name, age)
+
+	// 格式化输入
+	fmt.Print("请再次输入：")
+	fmt.Scanf("姓名 %s 年龄 %d", &name2, &age2)
+	fmt.Printf("输入的姓名是：%s,输入的年龄是%d\n", name2, age2)
+
 }
